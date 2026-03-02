64579f1ebc10cae4f5d9575c1f4835b9fd2e84c1
alifarhodov2102 <98308086+alifarhodov2102@users.noreply.github.com>
2026-03-02 15:46:45 -0500

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Enter your Telegram ID and any other admin IDs here
 ADMINS = [int(os.getenv("ADMIN_ID", 0))] 
 
+# Agarda Railway-da bo'lsa Volume-dan foydalanadi, bo'lmasa lokal baza
+DB_NAME = os.getenv("DB_PATH", "/app/data/bot_data.db")
+
 # Channel ID (e.g., -100123456789) where users must subscribe
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", 0))
 
